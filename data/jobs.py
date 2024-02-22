cec4e7377203fca5b4b6b6fdc1d083f6a2dfef98
Andrew2023 <andreybogdanov347@gmail.com>
2024-02-22 17:28:15 +0300

    Initial commit

---
+++ data/jobs.py
@@ -0,0 +1,17 @@
+import datetime
+import sqlalchemy
+from sqlalchemy.orm import session
+
+from .db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean, nullable=True)
---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from data import db_session
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
+
+
+@app.route('/')
+def index():
+    return 'фиг', 403
+
+
+if __name__ == '__main__':
+    db_session.global_init("./db/init.db")
+    user = User()
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+    app.run(port=80)
