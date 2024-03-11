74d9b13f0751172e2aae14dcb05f14183f316fa7
Jopkins <justfredbear@gmail.com>
2024-03-11 16:15:46 +0300

    edit api jobs

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import requests
 
-print(requests.get('http://127.0.0.1/api/jobs'))
-print(requests.get('http://127.0.0.1/api/jobs/1'))
-print(requests.get('http://127.0.0.1/api/jobs/-1'))
-print(requests.get('http://127.0.0.1/api/jobs/ss'))
+print(requests.get('http://127.0.0.1/api/jobs').json())
+print(requests.get('http://127.0.0.1/api/jobs/1').json())
+print(requests.get('http://127.0.0.1/api/jobs/-1').json())
+print(requests.get('http://127.0.0.1/api/jobs/ss').json())
 # 200
-print(requests.delete('http://127.0.0.1/api/jobs/3'))
+print(requests.delete('http://127.0.0.1/api/jobs/3').json())
 # 400
-print(requests.delete('http://127.0.0.1/api/jobs/ss'))
+print(requests.delete('http://127.0.0.1/api/jobs/ss').json())
 # 404
-print(requests.delete('http://127.0.0.1/api/jobs/1'))
+print(requests.delete('http://127.0.0.1/api/jobs/1').json())
